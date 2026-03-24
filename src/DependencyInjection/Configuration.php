37ebd295be90a4af863a403c9b11cdea5855be0f
Jurgo Touw <jurgo.touw@lyceo.nl>
2026-03-24 14:38:22 +0100

    Add support for Symfony 8

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,8 @@
 
 namespace Stogon\UnleashBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -11,44 +13,50 @@ final class Configuration implements ConfigurationInterface
 	{
 		$treeBuilder = new TreeBuilder('unleash_bundle');
 
+		/** @var ArrayNodeDefinition $rootNode */
 		$rootNode = $treeBuilder->getRootNode();
 
 		// @phpstan-ignore-next-line
-		$rootNode
-			->fixXmlConfig('unleash')
-			->children()
-				->scalarNode('api_url')
-					->info('Unleash API endpoint. URL Must end with a slash !')
-					->example('https://gitlab.com/api/v4/feature_flags/unleash/<project_id>/')
-					->isRequired()
-					->cannotBeEmpty()
-				->end()
-				->scalarNode('auth_token')
-					->info('Unleash auth token')
-					->cannotBeEmpty()
-				->end()
-				->scalarNode('instance_id')
-					->info('Unleash instance ID')
-					->isRequired()
-					->cannotBeEmpty()
-				->end()
-				->scalarNode('environment')
-					->info('Unleash application name. For Gitlab, it can be the environment name')
-					->defaultValue('%kernel.environment%')
-					->cannotBeEmpty()
-				->end()
-				->arrayNode('cache')
-					->canBeEnabled()
-					->info('Cache configurations for strategies')
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode('service')
-							->defaultNull()
-						->end()
-						->floatNode('ttl')
-							->min(0)
-							->defaultValue(15)
-						->end()
+		if (method_exists($rootNode, 'fixXmlConfig')) {
+			// @phpstan-ignore-next-line
+			$rootNode->fixXmlConfig('unleash');
+		}
+
+		$nodeBuilder = $rootNode->children();
+		assert($nodeBuilder instanceof NodeBuilder);
+
+		$nodeBuilder
+			->scalarNode('api_url')
+				->info('Unleash API endpoint. URL Must end with a slash !')
+				->example('https://gitlab.com/api/v4/feature_flags/unleash/<project_id>/')
+				->isRequired()
+				->cannotBeEmpty()
+			->end()
+			->scalarNode('auth_token')
+				->info('Unleash auth token')
+				->cannotBeEmpty()
+			->end()
+			->scalarNode('instance_id')
+				->info('Unleash instance ID')
+				->isRequired()
+				->cannotBeEmpty()
+			->end()
+			->scalarNode('environment')
+				->info('Unleash application name. For Gitlab, it can be the environment name')
+				->defaultValue('%kernel.environment%')
+				->cannotBeEmpty()
+			->end()
+			->arrayNode('cache')
+				->canBeEnabled()
+				->info('Cache configurations for strategies')
+				->addDefaultsIfNotSet()
+				->children()
+					->scalarNode('service')
+						->defaultNull()
+					->end()
+					->floatNode('ttl')
+						->min(0)
+						->defaultValue(15)
 					->end()
 				->end()
 			->end()
